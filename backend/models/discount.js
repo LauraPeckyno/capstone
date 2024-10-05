452622e80746216d34853abc40139177069711f0
LauraP <166152671+LauraPeckyno@users.noreply.github.com>
2024-10-05 14:32:10 -0400

    imported more data and had to adjust the models

--- backend/models/discount.js
+++ backend/models/discount.js
@@ -4,8 +4,14 @@ const discountSchema = new mongoose.Schema({
   business: { type: String, required: true },
   url: { type: String, required: true },
   discount: { type: String, required: true },
-  eligibility: { type: String, required: true },
+  eligibility: { type: String, required: false},
   category: { type: String, required: true },
+  address: { type: String, required: false },
+  city: { type: String, required: false },
+  state: { type: String, required: false },
+  zip: { type: String, required: false },
+  phone: { type: String, required: false }
+
 }, { timestamps: true });
 // Declares a new discount model
 
