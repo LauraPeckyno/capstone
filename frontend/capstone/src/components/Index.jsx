698b1cead2d00c53de4af9ae684b3966e667335d
LauraP <166152671+LauraPeckyno@users.noreply.github.com>
2024-10-09 20:48:52 -0400

    another typo

--- frontend/capstone/src/components/Index.jsx
+++ frontend/capstone/src/components/Index.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Featuredhome from "./Featuredhome";
 import SearchBar from "./Searchbar";
-import SearchResults from "./SearchResults";
+import SearchResults from "./Searchresults";
 
 function Index() {
   const [featuredDiscounts, setFeaturedDiscounts] = useState([]);
